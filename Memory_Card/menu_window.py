d321689483f4975f6c2e504fc16db6eb7efc53b6
Maksiko1a <maksym.kryvoruchko09@gmail.com>
2024-08-31 19:32:49 +0300

    Update menu_window.py

--- Memory_Card/menu_window.py
+++ Memory_Card/menu_window.py
@@ -1,54 +1,53 @@
-''' Головне вікно для картки запитання '''
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import *
-
+# ------------------------------- івафіваіф
+from PyQt5.QtCore import Qt      
+from PyQt5.QtWidgets import *  
+# ------------------------------- 
 
 
 app = QApplication([])
 
-#----------------------------------------------------------------------------------------------------------
-#Створення вікна
-#----------------------------------------------------------------------------------------------------------
+# ------------------------------- 
 menu_win = QWidget()
-menu_win.resize(400, 300)             #розмір вікна
-menu_win.setWindowTitle("Меню")    #назва вікна
+menu_win.resize(400, 300)            
+menu_win.setWindowTitle("Меню")
+# ------------------------------- 
 
-txt_Question = QLineEdit("")
-txt_Answer = QLineEdit("")
-txt_Wrong1 = QLineEdit("")
-txt_Wrong2 = QLineEdit("")
-txt_Whong3 = QLineEdit("")
+# Лінії для вводу нового запитання
+txt_Question = QLineEdit('')
+txt_Answer = QLineEdit('')
+txt_Wrong1 = QLineEdit('')
+txt_Wrong2 = QLineEdit('')
+txt_Wrong3 = QLineEdit('')
 
-
+# обєднання ліній та тексту
 layout_form = QFormLayout()
-layout_form.addRow("Питання:",txt_Question)
-layout_form.addRow("Правильна відповідь:",txt_Answer)
-layout_form.addRow("Неправильний варіант №1:",txt_Wrong1)
-layout_form.addRow("Неправильний варіант №2:",txt_Wrong2)
-layout_form.addRow("Неправильний варіант №3:",txt_Whong3)
 
-btn_add_q = QPushButton("Додати запитання")
-btn_back = QPushButton("Назад")
-btn_clear = QPushButton("Очистити")
+layout_form.addRow('Питання:', txt_Question)
+layout_form.addRow('Правильна відповідь:', txt_Answer)
+layout_form.addRow('Неправильний варіант №1:', txt_Wrong1)
+layout_form.addRow('Неправильний варіант №2', txt_Wrong2)
+layout_form.addRow('Неправильний варіант №3:', txt_Wrong3)
 
+# кнопки 
+btn_back = QPushButton('Назад')
+btn_add_q = QPushButton('Додати питання')
+btn_clear = QPushButton('Очистити')
 
-lbl_statistics = QLabel('')
+# Статистика
+lbl_statistics = QLabel('Статистика:\nПравильних відповідей: 0\nЗагальна кількість спроб: 0') 
 
-
-lbl_statistics = QLabel('Статистика:\nПравильних відповідей: 0\nЗагальну кількість спроб: 0')
-
-hbtn = QHBoxLayout()
+# гориз.лінія - макет
+hbtn = QHBoxLayout()         
 hbtn.addWidget(btn_back)
 hbtn.addWidget(btn_add_q)
 hbtn.addWidget(btn_clear)
 
-
+# Макет для основного вікна
 vline = QVBoxLayout()
 vline.addLayout(layout_form)
 vline.addLayout(hbtn)
-vline.addWidget(lbl_statistics)
+vline.addWidget(lbl_statistics) 
 
-menu_win.setLayout(vline)
+menu_win.setLayout(vline) # макет на вікно
 
-menu_win.show()
-app.exec_()
+
